a638348de98ad186e7f2489383f0a4dadefb6f74
Козлов Дмитрий Игоревич <Sib98@list.ru>
2017-03-21 12:27:31 +0700

    start

---
+++ coder.h
@@ -0,0 +1,13 @@
+#ifndef CODER_H
+#define CODER_H
+
+int encode(uint32_t code_point, CodeUnits *code_units)
+  if(){
+    return 0;
+  }else{
+    return -1;}
+uint32_t decode(const CodeUnit *code_unit)
+  if(){
+    return 0;
+  }else{
+    return -1;
---
+++ command.h
@@ -0,0 +1,13 @@
+#ifndef(COMMAND_H)
+#define(COMMAND_H)
+
+int encode_file(const char *in_file_name, const char *out_file_name)
+  if(){
+    return 0;
+  }else{
+    return -1;}
+int decode_file(const char *in_file_name, const char *out_file_name)
+  if(){
+    return 0;
+  }else{
+    return -1;
